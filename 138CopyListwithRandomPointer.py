8f11352d77d08242cd9667175384a9d11839466f
Bo Fan <bfan4@stevens.edu>
2019-02-19 04:12:44 -0500

    138 CopyListwithRandomPointer
    A linked list is given such that each node contains an additional random pointer which could point to any node in the list or null.
    
    Return a deep copy of the list.

--- 138CopyListwithRandomPointer.py
+++ 138CopyListwithRandomPointer.py
@@ -1,17 +1,22 @@
 class Solution(object):
 	def __init__(self):
+		# 创建一个名为visitedHash的map，注意局部变量加 self
 		self.visitedHash = {}
 
 	def copyRandomList(self, head):
 
+		#判断，如果当前节点是空，则返回空
 		if head == None:
 			return None
 
+		#判断，如果当前字段在visitedHash里存在，证明已经copy过了
 		if head in self.visitedHash:
 			return self.visitedHash[head]
 
-		node = RandomListNode[head.label]
+		#将当前节点head的label定义为新的node
+		node = RandomListNode(head.label)
 
+		#把他存进关键字为当前节点的map
 		self.visitedHash[head] = node
 
 		node.next = self.copyRandomList(head.next)
