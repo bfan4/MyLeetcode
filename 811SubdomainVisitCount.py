817b5c76c46a36c25df170d3cd295ba648ccc891
Bo Fan <bfan4@stevens.edu>
2019-02-26 21:44:06 -0500

    Add files via upload

---
+++ 811SubdomainVisitCount.py
@@ -0,0 +1,32 @@
+class Solution:
+	def subdomainVisits(self, cpdomains: List[str]) -> List[str]:
+		self.dice = {}
+		count = 0
+		domain = ''
+		for s in cpdomains:
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def main():
+	
+	ret = Solution().subdomainVisits(n)
+
+	print(ret)
+
+
+
+if __name__ == '__main__':
+	main()
