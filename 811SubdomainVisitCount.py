9396f0ea6e6540694be5f220ea217b2052ff3b40
Bo Fan <bfan4@stevens.edu>
2019-02-26 23:32:26 -0500

    Add files via upload

--- 811SubdomainVisitCount.py
+++ 811SubdomainVisitCount.py
@@ -1,9 +1,19 @@
+from collections import defaultdict
 class Solution:
-	def subdomainVisits(self, cpdomains: List[str]) -> List[str]:
-		self.dice = {}
-		count = 0
-		domain = ''
-		for s in cpdomains:
+    def subdomainVisits(self, cpdomains: List[str]) -> List[str]:
+        dic = defaultdict(int)
+        self.ans = []
+        for s in cpdomains:
+            num, addr = s.split(' ')
+            addr = addr.split('.')
+            while len(addr)>0:
+                web = '.'.join(addr)
+                
+                dic[web] += int(num)
+                addr = addr[1:]
+        for web, count in dic.items():
+            self.ans.append(str(count)+" "+web)
+        return self.ans
 
 
 
